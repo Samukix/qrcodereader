005987b387ed7a12b051a572f29bc0cede9a0fe0
Samuel Fernandes <150559807+Samukix@users.noreply.github.com>
2025-08-12 22:33:07 -0300

    Update resultado_cep.dart

--- lib/resultado_cep.dart
+++ lib/resultado_cep.dart
@@ -1,36 +1,27 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-
-import 'bloc/cep_bloc.dart';
-import 'bloc/cep_state.dart';
 
 class ResultPage extends StatelessWidget {
-  const ResultPage({super.key});
+  final String qrData;
+
+  const ResultPage({super.key, required this.qrData});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Confirmar QR Code', style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),), backgroundColor: Colors.red),
+      appBar: AppBar(
+        title: Text(
+          'Confirmar QR Code',
+          style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+        ),
+        backgroundColor: Colors.red,
+      ),
       backgroundColor: Colors.white,
       body: Padding(
         padding: EdgeInsets.all(16),
-        child: BlocBuilder<CepBloc, CepState>(
-          builder: (context, state) {
-              return Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-              Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text('CEP:', style: TextStyle(color: Colors.red, fontWeight: FontWeight.bold),),
-                      Text('Logradouro:', style: TextStyle(color: Colors.red, fontWeight: FontWeight.bold),),
-                      Text('Bairro:', style: TextStyle(color: Colors.red, fontWeight: FontWeight.bold),),
-                      Text('Cidade:', style: TextStyle(color: Colors.red, fontWeight: FontWeight.bold),),
-                      Text('UF:', style: TextStyle(color: Colors.red, fontWeight: FontWeight.bold),),
-                    ],
-              )]);
-          },
+        child: Text(
+          'Dados lidos: $qrData',
+          style: TextStyle(fontSize: 18),
         ),
       ),
     );
