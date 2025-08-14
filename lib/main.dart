c3fafa7d27cf9a681bbb8ca44cfca5c1370899bd
Samuel Fernandes <150559807+Samukix@users.noreply.github.com>
2025-08-13 22:08:28 -0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,21 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:dio/dio.dart';
-
-import 'bloc/cep_bloc.dart';
 import 'home_page.dart';
 
 void main() {
-  runApp(
-    BlocProvider(
-      create: (_) => CepBloc(Dio()),
-      child: const MyApp(),
-    ),
-  );
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Buscador de CEP',
-      home: QRcode(),
+      title: 'QR Code Veículo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.red),
+      ),
+      home: const QRcode(),
     );
   }
 }
